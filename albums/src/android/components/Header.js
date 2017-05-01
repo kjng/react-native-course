0cd46ab9d62538040477006aba095a9d49542304
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-30 20:01:48 -0700

    Add header bar and elevation

--- albums/src/android/components/Header.js
+++ albums/src/android/components/Header.js
@@ -1,17 +1,32 @@
 // Import libraries for making component
 import React from 'react';
-import { Text } from 'react-native';
+import { Dimensions, View, Text } from 'react-native';
 
 // Make the component
 const Header = () => {
-  const { headerText } = styles;
+  const { headerText, headerView } = styles;
 
-  return <Text style={headerText}>Albums</Text>;
+  return (
+    <View style={headerView}>
+      <Text style={headerText}>Albums</Text>
+    </View>
+  );
 };
 
+const { height } = Dimensions.get('window');
+
 const styles = {
+  headerView: {
+    backgroundColor: 'steelblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: height * 0.1,
+    elevation: 5, // Shadow for android 5.0+
+    position: 'relative'
+  },
   headerText: {
-    fontSize: 20
+    fontSize: 20,
+    color: 'white'
   }
 };
 
