c623614418f8518a9dbe41d81ee70034f1c6245d
Kevin Jang <pianistkevinjang@gmail.com>
2017-05-02 12:46:45 -0700

    Import firebase and initialize application

---
+++ auth/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyC3_--eqvHx1JzDlrsezLlocO5eZOgpYtg',
+  authDomain: 'rn-course-auth.firebaseapp.com',
+  databaseURL: 'https://rn-course-auth.firebaseio.com',
+  projectId: 'rn-course-auth',
+  storageBucket: 'rn-course-auth.appspot.com',
+  messagingSenderId: '106726547565'
+};
--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import config from '../firebaseConfig';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
