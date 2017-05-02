1ab2f98c9f3f24ba577ab629b83ab2cdbb89a48c
Kevin Jang <pianistkevinjang@gmail.com>
2017-05-01 20:29:20 -0700

    Add artistContent and artistImage content

--- albums/src/android/components/AlbumDetail.js
+++ albums/src/android/components/AlbumDetail.js
@@ -1,18 +1,61 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-  const { title, artist, url, image, thumbnail_image } = props.album;
+const AlbumDetail = ({ album }) => {
+  const { title, artist, url, image, thumbnail_image } = album;
+  const { artistContentStyle, artistImageStyle, artistImageContainerStyle, artistContentTitleStyle, albumImageStyle } = styles;
 
   return (
     <Card>
       <CardSection>
-        <Text>{title}</Text>
+        <View style={artistImageContainerStyle}>
+          <Image
+            style={artistImageStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={artistContentStyle}>
+          <Text style={artistContentTitleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={albumImageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  artistContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  artistContentTitleStyle: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  },
+  artistImageStyle: {
+    height: 50,
+    width: 50
+  },
+  artistImageContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft: 5
+  },
+  albumImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
