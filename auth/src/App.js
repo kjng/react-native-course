54e613d0adc5aae3d93c0eb42afae579a1a28507
Kevin Jang <pianistkevinjang@gmail.com>
2017-05-02 16:46:45 -0700

    Add Log out button, spinner, and small refactors

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,22 +2,68 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import config from '../firebaseConfig';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: null };
+  }
+
   componentDidMount() {
     firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    const { spinnerContainer, buttonContainer } = styles;
+
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={buttonContainer}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={spinnerContainer}>
+            <Spinner size={'large'} />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header title={'Authentication'} />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  buttonContainer: {
+    flexDirection: 'row'
+  },
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+};
 
 export default App;
